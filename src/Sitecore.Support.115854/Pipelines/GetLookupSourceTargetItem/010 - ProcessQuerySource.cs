d890d1a884e40a9152b9e062fd83549e6bf8f080
Nikolay Mitikov <nmi@sitecore.net>
2017-04-13 10:38:56 +0800

    #115854: Add GetLookupSourceTargetItem processors

---
+++ src/Sitecore.Support.115854/Pipelines/GetLookupSourceTargetItem/010 - ProcessQuerySource.cs
@@ -0,0 +1,51 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright company="Sitecore Corporation" file="010 - ProcessQuerySource.cs">
+//   Copyright (C) 2017 by Sitecore Corporation
+// </copyright>
+// <summary>
+//   Handles target item resolution when query source is Sitecore Query (starts with 'query:' prefix).
+// </summary>
+// -------------------------------------------------------------------------------------------------------------------- 
+namespace Sitecore.Support.Pipelines.GetLookupSourceTargetItem
+{
+  using Sitecore.Diagnostics;  
+
+  /// <summary>
+  ///   Processes query sources that start with 'query:' prefix.
+  /// </summary>
+  public class ProcessQuerySource : GetLookupSourceItems.ProcessQuerySource
+  {
+    #region Public methods
+
+    /// <summary>
+    ///   Validates the lookup source link for Sitecore query format.
+    /// <para>Appends field value to query, and computes it. Sets computed item as pipeline execution result, and aborts it.</para>
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public virtual void Execute([NotNull] GetLookupSourceTargetItemArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      
+      if (string.IsNullOrEmpty(args.Source) || string.IsNullOrEmpty(args.Value))
+      {
+        return;
+      }
+
+      bool isFastQuery;
+      string sitecoreQuery;
+
+      if (!this.IsSitecoreQuery(args.Source, out sitecoreQuery, out isFastQuery))
+      {
+        return;
+      }
+
+      sitecoreQuery = string.Concat(sitecoreQuery, '/', args.Value);
+
+      var targetItem = isFastQuery ? args.Item.Database.SelectSingleItem(sitecoreQuery) : args.Item.Axes.SelectSingleItem(sitecoreQuery);
+      
+      args.SetResultAndAbort(targetItem);
+    }
+
+    #endregion
+  }
+}
---
+++ src/Sitecore.Support.115854/Pipelines/GetLookupSourceTargetItem/060 - GetAccountsDataSource.cs
@@ -0,0 +1,44 @@
+﻿namespace Sitecore.Support.Pipelines.GetLookupSourceTargetItem
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Social.Infrastructure;
+
+  public class GetAccountsDataSource : Social.Client.Pipelines.GetLookupSourceItems.GetAccountsDataSource
+  {
+    /// <summary>    
+    /// <para>Processes social-related links using <see cref="GetAccountsDataSource"/> code.</para>    
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public virtual void Execute([NotNull] GetLookupSourceTargetItemArgs args)
+    {
+      if (!ExecutingContext.Current.IoCState.IoCLoaded())
+      {
+        return;
+      }
+
+      Assert.ArgumentNotNull(args, nameof(args));
+
+      var lookupArgs = args.ToLookupSourceItemArgs();
+
+      // TODO: Think about better approach instead of loading all items.
+      this.Process(lookupArgs);
+
+      foreach (Item lookupTargetItem in lookupArgs.Result)
+      {
+        if (this.Matches(lookupTargetItem, args))
+        {
+          args.SetResultAndAbort(lookupTargetItem);
+        }
+      }      
+    }
+
+    /// <summary>
+    /// Checks if the specified lookup target item matches value from args.
+    /// </summary>
+    /// <param name="lookupTargetItem">The lookup target item.</param>
+    /// <param name="args">The arguments.</param>
+    /// <returns></returns>
+    protected virtual bool Matches([NotNull] Item lookupTargetItem, [NotNull] GetLookupSourceTargetItemArgs args) => lookupTargetItem.Name == args.Value; // TODO: test
+  }
+}
