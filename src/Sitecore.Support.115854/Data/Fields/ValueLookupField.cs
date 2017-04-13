cf66d210aa8f35cbdd6c10bb9db48659675b73fc
Nikolay Mitikov <nmi@sitecore.net>
2017-04-13 10:42:03 +0800

    #115854:Consume new pipeline for links validation

--- src/Sitecore.Support.115854/Data/Fields/ValueLookupField.cs
+++ src/Sitecore.Support.115854/Data/Fields/ValueLookupField.cs
@@ -1,18 +1,15 @@
 ﻿namespace Sitecore.Support.Data.Fields
 {
-  using Sitecore.Configuration;
-  using Sitecore.Data;
   using Sitecore.Data.Fields;
   using Sitecore.Diagnostics;
   using Sitecore.Links;
-  using Sitecore.Text;
-  using Sitecore.Web.UI.HtmlControls.Data;
+  using Sitecore.Support.Pipelines.GetLookupSourceTargetItem;
 
   public class ValueLookupField : Sitecore.Data.Fields.ValueLookupField
   {
     [UsedImplicitly]
     public ValueLookupField(Field innerField) : base(innerField)
-    {              
+    {
     }
 
     /// <summary>
@@ -21,95 +18,26 @@
     /// <param name="result">The result.</param>
     public override void ValidateLinks(LinksValidationResult result)
     {
-      Assert.ArgumentNotNull(result, nameof(result));
-      var value = Value;
-      if (string.IsNullOrEmpty(value))
+      Assert.ArgumentNotNull(result, "result");
+
+      string fieldValue = this.Value;
+      string fieldSource = this.InnerField.Source;
+
+      if (string.IsNullOrEmpty(fieldValue) || string.IsNullOrEmpty(fieldSource))
       {
         return;
       }
 
-      if (InnerField.Source.Length == 0)
+      var targetItem = GetLookupSourceTargetItemPipeline.GetTargetItem(this.InnerField.Item, fieldSource, fieldValue);
+
+      if (targetItem != null)
       {
-        return;
+        result.AddValidLink(targetItem, targetPath: fieldValue);        
       }
-      var item = InnerField.Item;
-      var datasource = InnerField.Source;
-      if (LookupSources.IsComplex(InnerField.Source))
+      else
       {
-        var source = new UrlString(InnerField.Source);
-        var databaseName = source["databasename"];
-        if (!string.IsNullOrEmpty(databaseName))
-        {
-          var database = Factory.GetDatabase(databaseName);
-          Assert.IsNotNull(database, $"{nameof(database)}: {databaseName}");
-
-          item = database.GetRootItem(base.InnerField.Item.Language);
-          Assert.IsNotNull(item, $"Root item in database \"{databaseName}\" not found");
-        }
-
-        datasource = source["datasource"];
-      }
-
-      var sources = datasource.Split('|');
-
-      //string[] array2 = array;
-      for (var i = 0; i < sources.Length; i++)
-      {
-        var source = sources[i];
-        if (ID.IsID(source))
-        {
-          var item2 = item.Database.GetItem(source);
-          if (item2 == null)
-          {
-            continue;
-          }
-
-          var targetItem = item.Database.GetItem(item2.Paths.FullPath + '/' + value);
-          if (targetItem == null)
-          {
-            continue;
-          }
-
-          result.AddValidLink(targetItem, value);
-
-          return;
-          //ChildList children = item2.Children;
-          //if (children.Count > 0)
-          //{
-          //    args.Result.AddRange(children.ToArray());
-          //}
-        }
-        else
-        {
-          if (source.StartsWith("/", System.StringComparison.InvariantCulture) && !source.StartsWith("/sitecore", System.StringComparison.OrdinalIgnoreCase) && !source.StartsWith("/" + ItemIDs.RootID, System.StringComparison.OrdinalIgnoreCase))
-          {
-            source = "/sitecore" + source;
-          }
-
-          var targetItem = item.Database.GetItem(source + '/' + value);
-          if (targetItem == null)
-          {
-            continue;
-          }
-
-          result.AddValidLink(targetItem, value);
-          return;
-        }
-      }
-          
-      //Item[] items = LookupSources.GetItems(base.InnerField.Item, base.InnerField.Source);
-      //Item[] array = items;
-      //for (int i = 0; i < array.Length; i++)
-      //{
-      //    Item item = array[i];
-      //    if (value == item.Name)
-      //    {
-      //        result.AddValidLink(item, value);
-      //        return;
-      //    }
-      //}
-
-      result.AddBrokenLink(value);
+        result.AddBrokenLink(targetPath: fieldValue);
+      }      
     }
   }
 }
